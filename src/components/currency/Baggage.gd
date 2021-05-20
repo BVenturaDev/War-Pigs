67f1d325232c8a53ecd05b14bd893b589faa30f8
Car <anakin_chato_@hotmail.com>
2021-05-20 10:31:20 -0500

    Added check to avoid adding another coin

--- src/components/currency/Baggage.gd
+++ src/components/currency/Baggage.gd
@@ -3,8 +3,11 @@ extends Spatial
 var currency
 
 func give_currency(c):
-	currency = c
-	self.add_child(currency)
+	if currency != null:
+		print_debug("WARNING: Adding another coin")
+	else:
+		currency = c
+		self.add_child(currency)
 	
 func remove_currency() -> CurrencyData:
 	self.remove_child(currency)
