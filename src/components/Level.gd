c4d10b708dd8a4a44d55a2802b60b7398c7664a0
Ben Ventura <oeslian@gmail.com>
2021-05-21 22:43:31 -0400

    Minor Tweaks

--- src/components/Level.gd
+++ src/components/Level.gd
@@ -3,9 +3,12 @@ extends Spatial
 export (bool) var DEBUG = false
 
 var pig_scene = preload("res://scenes/components/Minion.tscn")
-
+var shop: bool = false
 
 func _ready():
+	if name == "Shop":
+		shop = true
+		
 	for x in Globals.total_pigs:
 		var pig = pig_scene.instance()
 			
