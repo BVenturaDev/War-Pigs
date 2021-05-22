a2a7861834418ae68e40d8b945b7638bba9fe8e0
Cyril Bos <cbos.pro@gmail.com>
2021-05-23 00:40:14 +0200

    animated fade in and out for the different music layers

--- src/audio/music_manager.gd
+++ src/audio/music_manager.gd
@@ -6,52 +6,61 @@ const MARCH_MINIMUM_PIGS_THIRD_LAYER = 10
 const WAR_MINIMUM_PIGS_SECOND_LAYER = 5
 const WAR_MINIMUM_PIGS_THIRD_LAYER = 10
 
-onready var _current_music = $March
+onready var _march: Node = $March
+onready var _war: Node = $War
 var _is_march_on = true
+
+var last_pig_count = 0
+var last_combat_pig_count = 0
 
 
 func _ready() -> void:
 	Globals.connect("total_pigs_updated", self, "_update_march_layers")
 	Globals.connect("combat_pigs_updated", self, "_handle_track_switch")
+	
+
+func _fade_in_layer(music_node: Node, layer_name: String):
+	music_node.get_node(layer_name).get_node("AnimationPlayer").play("fade_in")
 
 
-func _set_layer_volume(layer_name: String, volume: float) -> void:
-	for music_layer in _current_music.get_node(layer_name).get_children():
-		music_layer.set_volume_db(linear2db(1))
+func _fade_out_layer(music_node: Node, layer_name: String):
+	music_node.get_node(layer_name).get_node("AnimationPlayer").play("fade_out")
 
 
 func _update_march_layers(total_pigs: int) -> void:
 	if _is_march_on:
-		_update_music_layers(total_pigs, MARCH_MINIMUM_PIGS_SECOND_LAYER, MARCH_MINIMUM_PIGS_THIRD_LAYER)
+		_update_music_layers(_march, total_pigs, last_pig_count, MARCH_MINIMUM_PIGS_SECOND_LAYER, MARCH_MINIMUM_PIGS_THIRD_LAYER)
+		last_pig_count = total_pigs
 
 
-func _update_music_layers(total_pigs: int, first_threshold: int, second_threshold: int) -> void:
-	if total_pigs >= first_threshold:
-		_set_layer_volume("SecondLayer", 1.0)
-	elif total_pigs >= second_threshold:
-		_set_layer_volume("SecondLayer", 1.0)
-		_set_layer_volume("ThirdLayer", 1.0)
+func _update_music_layers(music_node: Node, total_pigs: int, last_pig_count: int, first_threshold: int, second_threshold: int) -> void:
+	if total_pigs == first_threshold:
+		_fade_in_layer(music_node, "SecondLayer")
+	elif total_pigs == second_threshold:
+		_fade_in_layer(music_node, "ThirdLayer")
 	else:
-		_set_layer_volume("SecondLayer", 0.0)
-		_set_layer_volume("ThirdLayer", 0.0)
+		if last_pig_count >= second_threshold and total_pigs < second_threshold:
+			_fade_out_layer(music_node, "ThirdLayer")
+		if last_pig_count >= first_threshold and total_pigs < first_threshold:
+			_fade_out_layer(music_node, "SecondLayer")
 
 
 func _handle_track_switch(total_combat_pigs: int) -> void:
 	if _is_march_on:
 		if total_combat_pigs > 0:
-			_set_layer_volume("MainLayer", 0.0)
-			_set_layer_volume("SecondLayer", 0.0)
-			_set_layer_volume("ThirdLayer", 0.0)
-			_current_music = $War
+			_fade_out_layer(_march, "MainLayer")
+			if Globals.total_pigs >= MARCH_MINIMUM_PIGS_SECOND_LAYER:
+				_fade_out_layer(_march, "SecondLayer")
+			if Globals.total_pigs >= MARCH_MINIMUM_PIGS_THIRD_LAYER:
+				_fade_out_layer(_march, "ThirdLayer")
 			_is_march_on = false
-			_set_layer_volume("MainLayer", 1.0)
+			_fade_in_layer(_war, "MainLayer")
 	else:
 		if total_combat_pigs == 0:
-			_set_layer_volume("MainLayer", 0.0)
-			_set_layer_volume("SecondLayer", 0.0)
-			_set_layer_volume("ThirdLayer", 0.0)
-			_current_music = $March
+			_fade_out_layer(_war, "MainLayer")
 			_is_march_on = true
-			_set_layer_volume("MainLayer", 1.0)
+			_fade_in_layer(_march, "MainLayer")
+		else:
+			_update_music_layers(_war, total_combat_pigs, last_combat_pig_count, WAR_MINIMUM_PIGS_SECOND_LAYER, WAR_MINIMUM_PIGS_THIRD_LAYER)
+	last_combat_pig_count = total_combat_pigs
 
-	_update_music_layers(total_combat_pigs, WAR_MINIMUM_PIGS_SECOND_LAYER, WAR_MINIMUM_PIGS_THIRD_LAYER)
